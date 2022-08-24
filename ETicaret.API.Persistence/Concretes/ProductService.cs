4612c161df3d386ef46eab88fad9cae2b3b0d541
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-08-24 22:54:11 +0300

    Add project files.

---
+++ ETicaret.API.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.API.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new() { new() { Id = Guid.NewGuid(), CreatedDate = DateTime.UtcNow, Name = "Elma", Price = 5, Stock = 10 } };
+
+    }
+}
