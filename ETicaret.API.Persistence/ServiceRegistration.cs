d15fc50a500b8aa86bd664de60bd8e851e0d6702
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-08-25 23:08:38 +0300

    25/08/2022 -2

--- ETicaret.API.Persistence/ServiceRegistration.cs
+++ ETicaret.API.Persistence/ServiceRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaret.API.Persistence.Concretes;
+using ETicaret.API.Persistence.Contexts;
 using ETicaretAPI.Application.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -6,14 +7,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ETicaret.API.Persistence
 {
     public static class ServiceRegistration
     {
-        public static void AddPersistenceServices(this IServiceCollection services)
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseSqlServer
+            (configuration.GetConnectionString("SqlServerConnectionStrings")));
         }
     }
 }
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -4,9 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddPersistenceServices();
-
 builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
